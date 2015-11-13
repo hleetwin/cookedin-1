ebe226aa7ca410799b02c13c0ad4abb6d822501e
S Skiba <ss.skiba.naut@gmail.com>
2015-11-13 09:52:57 -0800

    create job.service.js

---
+++ client/components/job/job.service.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('cookedinApp')
+  .factory('Job', function ($resource) {
+    return $resource('/api/jobs/:id/:controller', {
+      id: '@_id'
+    },
+    {
+      changePassword: {
+        method: 'PUT',
+        params: {
+          controller:'password'
+        }
+      },
+      get: {
+        method: 'GET',
+        params: {
+          id:'me'
+        }
+      }
+	  });
+  });
